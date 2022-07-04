011075072b6f49f332e4fa14ff438db5eb64a218
RAHUL JAYSAWAL <91250891+RahulJaysawal@users.noreply.github.com>
2022-07-04 18:43:09 +0530

    Add files via upload

---
+++ Days-06/ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+     int n= prices.size();
+        int maxi = 0;
+        int mini = INT_MAX;
+        for(int i =0;i<n;i++){
+            mini = min(prices[i],mini);
+            maxi= max(maxi,prices[i] - mini);
+        }
+        return maxi;
+}
