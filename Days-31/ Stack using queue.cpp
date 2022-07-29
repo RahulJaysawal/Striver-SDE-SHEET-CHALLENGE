577bd10a572a5605e5e64f53425eb63ea348dc0b
RAHUL JAYSAWAL <91250891+RahulJaysawal@users.noreply.github.com>
2022-07-29 23:12:44 +0530

    Add files via upload

---
+++ Days-31/ Stack using queue.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+class Stack {
+    // Define the data members.
+queue<int> q;
+   public:
+    Stack() {
+        // Implement the Constructor.
+        
+    }
+
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function.
+        return q.size();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function.
+        return (q.empty());
+    }
+
+    void push(int element) {
+        // Implement the push() function.
+        int s=q.size();
+        q.push(element);
+        for(int i=0;i<s;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        // Implement the pop() function.
+        if(!q.empty()){
+            int element=q.front();
+            q.pop();
+            return element;
+        }
+        return -1;
+    }
+
+    int top() {
+        // Implement the top() function.
+        if(!q.empty()){
+             return  q.front();
+        }
+     return -1;
+    }
+};
